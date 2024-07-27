aefda7fbcf131ac94763ae62d77e3e750b3448bb
prathamo <prathamesh.more@amdocs.com>
2024-07-27 16:42:18 +0530

    fix

--- EKS_cluster/providers.tf
+++ EKS_cluster/providers.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 locals {
   clusterregion = "ap-south-1"
   clustername = "myeks-cluster"
