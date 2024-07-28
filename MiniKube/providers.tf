75f2f6b2df6567b1f8068537bfe8039138490a33
prathamo <prathamesh.more@amdocs.com>
2024-07-28 17:27:15 +0530

    fix

--- MiniKube/providers.tf
+++ MiniKube/providers.tf
@@ -6,8 +6,8 @@ provider "helm" {
 }
 
 
-resource "helm_release" "gitlabrunner-agent1" {
-  name       = "mygitlab-runner"
+resource "helm_release" "gitlabrunner-agent11" {
+  name       = "mygitlab-runner1"
   repository = "https://charts.gitlab.io"
   chart      = "gitlab-runner"
   version    = "0.64.0"
